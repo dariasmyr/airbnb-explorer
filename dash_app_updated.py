db08bd8fe1e886ebdaff18b0625e15a51774a318
daria_smyr <dasha.smyr@gmail.com>
2023-03-19 01:50:23 +0300

    Improved dash_app_updated.py

--- dash_app_updated.py
+++ dash_app_updated.py
@@ -4,9 +4,15 @@
 import plotly.express as px
 from dash import Dash, dcc, html
 from modules.database_repository import Database
+import dash_bootstrap_components as dbc
+
+external_stylesheets = [
+    dbc.themes.BOOTSTRAP,
+    'https://fonts.googleapis.com/css?family=Roboto+Slab&display=swap'
+]
 
 # Initialize the app
-app = Dash(__name__)
+app = Dash(__name__, external_stylesheets=external_stylesheets)
 
 # Initialize the database connection
 database = Database("sqlite+pysqlite:///:/../data/data.sqlite3")
@@ -27,12 +33,13 @@ style = {
         'backgroundColor': colors['background'],
         'boxShadow': '0px 0px 20px 0px rgba(0,0,0,0.05)',
         'border-radius': '20px',
+        'fontFamily': 'Roboto Slab, sans-serif',
     },
     'title': {
         'font': {
-            'size': 28,
-            'family': 'Arial, sans-serif',
-            'color': colors['text']
+            'size': 20,
+            'color': colors['text'],
+            'family': 'Roboto Slab, sans-serif'
         },
         'x': 0.5,
         'y': 0.9,
@@ -40,15 +47,15 @@ style = {
     'axis_title': {
         'font': {
             'size': 18,
-            'family': 'Arial, sans-serif',
-            'color': colors['text']
+            'color': colors['text'],
+            'family': 'Roboto Slab, sans-serif'
         },
     },
     'text': {
         'color': colors['text'],
         'font': {
             'size': 24,
-            'family': 'Arial',
+            'family': 'Roboto Slab, sans-serif'
         },
     },
     'h1': {
@@ -56,8 +63,8 @@ style = {
         'color': colors['text'],
         'font': {
             'size': 48,
-            'family': 'Arial, sans-serif',
-            'weight': 'bold'
+            'weight': 'bold',
+            'family': 'Roboto Slab, sans-serif'
         },
         'padding': '40px 0px 20px 20px'
     },
@@ -105,6 +112,39 @@ mean_price_per_neighbourhood_fig.update_layout(
     paper_bgcolor=colors['background'],
 )
 
+numeric_cols = ['price', 'minimum_nights', 'number_of_reviews', 'reviews_per_month', 'availability_365']
+
+correlation_with_price = df[numeric_cols].corr()['price'].sort_values(ascending=False)
+
+# Create the bar chart
+correlation_with_price_fig = px.bar(
+    correlation_with_price,
+    x=correlation_with_price.values,
+    y=correlation_with_price.index,
+    orientation='h',
+    color_discrete_sequence=[colors['accent']],
+)
+
+# Customize the layout of the chart
+correlation_with_price_fig.update_layout(
+    title={
+        'text': 'Correlation with price',
+        **style['title']
+    },
+    xaxis_title={
+        'text': 'Correlation',
+        **style['axis_title']
+    },
+    yaxis_title={
+        'text': 'Feature',
+        **style['axis_title']
+    },
+    yaxis_range=[correlation_with_price.index[-1], correlation_with_price.index[0]],
+    margin=dict(t=100),
+    plot_bgcolor=colors['background'],
+    paper_bgcolor=colors['background'],
+)
+
 # Define the app layout
 app.layout = html.Div(style=style['page'], children=[
     html.Div([
@@ -131,6 +171,13 @@ app.layout = html.Div(style=style['page'], children=[
         html.P(
             children='This project is part of the Data Science for Social Good program at the University of Zurich.',
             style=style['p']
+        ),
+    ]),
+    html.Div([
+        dcc.Graph(
+            id='correlation_with_price',
+            figure=correlation_with_price_fig,
+            style=style['graph']
         ),
     ]),
 ])
