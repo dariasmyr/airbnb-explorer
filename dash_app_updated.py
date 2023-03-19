8e0f92eb3e156373bce27f16f9f754bb327f4cda
daria_smyr <dasha.smyr@gmail.com>
2023-03-19 14:50:17 +0300

    Improved dash_app_updated.py

--- dash_app_updated.py
+++ dash_app_updated.py
@@ -27,7 +27,7 @@ colors = {
 
 style = {
     'page': {
-        'margin': '0px 200px 0px 200px',
+        'margin': '40px 200px 0px 200px',
         'maxWidth': '1200px',
         'padding': '0px 0px 0px 0px',
         'backgroundColor': colors['background'],
@@ -66,24 +66,41 @@ style = {
             'weight': 'bold',
             'family': 'Roboto Slab, sans-serif'
         },
-        'padding': '40px 0px 20px 20px'
+        'padding': '40px 0px 10px 20px'
+    },
+    'h2': {
+        'textAlign': 'left',
+        'color': colors['text'],
+        'font': {
+            'size': 36,
+            'weight': 'bold',
+            'family': 'Roboto Slab, sans-serif'
+        },
+        'padding': '15px 0px 15px 20px'
+    },
+    'h3': {
+        'textAlign': 'left',
+        'color': colors['text'],
+        'font': {
+            'size': 24,
+            'weight': 'bold',
+            'family': 'Roboto Slab, sans-serif'
+        },
+        'padding': '0px 0px 10px 20px'
     },
     'p': {
         'textAlign': 'left',
-        'padding': '0px 0px 20px 20px'
+        'padding': '0px 0px 10px 20px'
     },
     'graph': {
         'padding': '0px 20px 20px 20px'
     }
 }
 
-# Retrieve data from the database
 df = database.get_dataframe()
 
-# Compute the mean price per neighbourhood group
 mean_price_per_neighbourhood = df.groupby('neighbourhood_group')['price'].mean().sort_values(ascending=False)
 
-# Create the bar chart
 mean_price_per_neighbourhood_fig = px.bar(
     mean_price_per_neighbourhood,
     x=mean_price_per_neighbourhood.values,
@@ -92,7 +109,6 @@ mean_price_per_neighbourhood_fig = px.bar(
     color_discrete_sequence=[colors['accent']],
 )
 
-# Customize the layout of the chart
 mean_price_per_neighbourhood_fig.update_layout(
     title={
         'text': 'Average price of Airbnb listings in each neighbourhood group',
@@ -116,7 +132,6 @@ numeric_cols = ['price', 'minimum_nights', 'number_of_reviews', 'reviews_per_mon
 
 correlation_with_price = df[numeric_cols].corr()['price'].sort_values(ascending=False)
 
-# Create the bar chart
 correlation_with_price_fig = px.bar(
     correlation_with_price,
     x=correlation_with_price.values,
@@ -125,7 +140,6 @@ correlation_with_price_fig = px.bar(
     color_discrete_sequence=[colors['accent']],
 )
 
-# Customize the layout of the chart
 correlation_with_price_fig.update_layout(
     title={
         'text': 'Correlation with price',
@@ -140,6 +154,37 @@ correlation_with_price_fig.update_layout(
         **style['axis_title']
     },
     yaxis_range=[correlation_with_price.index[-1], correlation_with_price.index[0]],
+    margin=dict(t=100),
+    plot_bgcolor=colors['background'],
+    paper_bgcolor=colors['background'],
+)
+
+percentage_of_available_listings_from_each_neighbourhood_group = df.groupby('neighbourhood_group')['minimum_nights']. \
+    apply(lambda x: (x >= 10).sum() / len(x) * 100).sort_values(ascending=False)
+
+percentage_of_available_listings_from_each_neighbourhood_group_fig = px.bar(
+    percentage_of_available_listings_from_each_neighbourhood_group,
+    x=percentage_of_available_listings_from_each_neighbourhood_group.values,
+    y=percentage_of_available_listings_from_each_neighbourhood_group.index,
+    orientation='h',
+    color_discrete_sequence=[colors['accent']],
+)
+
+percentage_of_available_listings_from_each_neighbourhood_group_fig.update_layout(
+    title={
+        'text': 'Percentage of available listings from each neighbourhood group',
+        **style['title']
+    },
+    xaxis_title={
+        'text': 'Percentage',
+        **style['axis_title']
+    },
+    yaxis_title={
+        'text': 'Neighbourhood group',
+        **style['axis_title']
+    },
+    yaxis_range=[percentage_of_available_listings_from_each_neighbourhood_group.index[-1],
+                 percentage_of_available_listings_from_each_neighbourhood_group.index[0]],
     margin=dict(t=100),
     plot_bgcolor=colors['background'],
     paper_bgcolor=colors['background'],
@@ -161,6 +206,22 @@ app.layout = html.Div(style=style['page'], children=[
         ),
     ]),
     html.Div([
+        html.H2(
+            children='Price',
+            style=style['h2']
+        ),
+        html.H3(
+            children='Mean price per neighbourhood group',
+            style=style['h3']
+        ),
+        html.P(
+            children='The average price of Airbnb listings in each neighbourhood group is shown in the graph below. '
+                     'The most expensive neighbourhood group is Manhattan, followed by Brooklyn and Queens. '
+                     'The cheapest neighbourhood group is Bronx.',
+            style=style['p']
+        ),
+    ]),
+    html.Div([
         dcc.Graph(
             id='mean_price_per_neighbourhood',
             figure=mean_price_per_neighbourhood_fig,
@@ -168,8 +229,16 @@ app.layout = html.Div(style=style['page'], children=[
         ),
     ]),
     html.Div([
+        html.H3(
+            children='Correlation with price',
+            style=style['h3']
+        ),
         html.P(
-            children='This project is part of the Data Science for Social Good program at the University of Zurich.',
+            children='The correlation between the price of an Airbnb listing and other features is shown in the '
+                     'graph below. '
+                     'The most important features for determining the price of an Airbnb listing are the number '
+                     'of reviews, the availability of the listing, and the number of reviews per month. '
+                     'The least important feature is the minimum number of nights.',
             style=style['p']
         ),
     ]),
@@ -177,6 +246,31 @@ app.layout = html.Div(style=style['page'], children=[
         dcc.Graph(
             id='correlation_with_price',
             figure=correlation_with_price_fig,
+            style=style['graph']
+        ),
+    ]),
+    html.Div([
+        html.H2(
+            children='Availability',
+            style=style['h2']
+        ),
+        html.H3(
+            children='Percentage of available listings from each neighbourhood group',
+            style=style['h3']
+        ),
+        html.P(
+            children='The percentage of available listings from each neighbourhood group is shown in the graph '
+                     'below. '
+                     'The neighbourhood group with the highest percentage of available listings is Staten '
+                     'Island, followed by Bronx and Queens. '
+                     'The neighbourhood group with the lowest percentage of available listings is Manhattan.',
+            style=style['p']
+        ),
+    ]),
+    html.Div([
+        dcc.Graph(
+            id='percentage_of_available_listings_from_each_neighbourhood_group',
+            figure=percentage_of_available_listings_from_each_neighbourhood_group_fig,
             style=style['graph']
         ),
     ]),
